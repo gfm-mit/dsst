28ca8db3266bf466d536b862530bf635ad5cefe8
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-04-22 12:34:10 -0400

    refactor ROC code
    Clean up names and types, and separate into roc.py

---
+++ plot/roc.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from scipy.stats import norm
+from scipy.spatial import ConvexHull
+import numpy as np
+
+def get_roc(predicted, labels):
+  points = pd.DataFrame(dict(predicted=predicted, labels=labels)).sort_values(by="predicted")
+  points["tpr"] = 1 - (1+points.labels.cumsum() - points.labels) / (2+points.labels.sum())
+  negs = 1 - points.labels
+  points["fpr"] = 1 - (1+negs.cumsum() - negs) / (2+negs.sum())
+  points["tpr_literal"] = 1 - (points.labels.cumsum() - points.labels) / (points.labels.sum())
+  negs = 1 - points.labels
+  points["fpr_literal"] = 1 - (negs.cumsum() - negs) / (negs.sum())
+  return points
+
+def get_roc_convex_hull(_, fpr, tpr):
+  L = 0
+  H = 1
+  fpr = np.concatenate([[L, H, H], fpr])
+  tpr = np.concatenate([[L, L, H], tpr])
+  idx = ConvexHull(
+      np.stack([fpr, tpr]).T,
+      ).vertices
+  idx = np.roll(idx, -1)[1:]
+  fpr = fpr[idx]
+  tpr = tpr[idx]
+  return pd.DataFrame(dict(fpr=fpr, tpr=tpr))
+
+def interpolate(f1, f2, t1, t2):
+  N = 100
+  return np.stack([
+    np.linspace(f1, f2, N), np.linspace(t1, t2, N)
+  ]).T
+
+def get_roc_interpolated_convex_hull(fpr, tpr):
+  fn = iter(fpr)
+  next(fn)
+  tn = iter(tpr)
+  next(tn)
+  points = np.concatenate([
+    interpolate(f1, f2, t1, t2)
+    for f1, f2, t1, t2 in zip(fpr, fn, tpr, tn)
+  ]).T
+  fpr, tpr = points
+  return pd.DataFrame(dict(fpr=fpr, tpr=tpr))
--- plot/swets.py
+++ plot/swets.py
@@ -2,25 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from plot.probit import *
 from plot.log_one_minus import *
+from plot.roc import *
 from scipy.stats import norm
 from scipy.spatial import ConvexHull
 
-def get_roc(predicted, labels):
-  points = pd.DataFrame(dict(predicted=predicted, labels=labels)).sort_values(by="predicted")
-  points["tpr"] = 1 - (1+points.labels.cumsum() - points.labels) / (2+points.labels.sum())
-  negs = 1 - points.labels
-  points["fpr"] = 1 - (1+negs.cumsum() - negs) / (2+negs.sum())
-  points["tpr_literal"] = 1 - (points.labels.cumsum() - points.labels) / (points.labels.sum())
-  negs = 1 - points.labels
-  points["fpr_literal"] = 1 - (negs.cumsum() - negs) / (negs.sum())
-  return points
-
-def plot_swets_roc(predicted, labels, axs):
-  points = get_roc(predicted, labels)
+def plot_9_types(predicted, labels, axs):
+  roc = get_roc(predicted, labels)
+  convex = get_roc_convex_hull(roc.shape[0], roc.fpr_literal.values, roc.tpr_literal.values)
+  interpolated_smooth = get_roc_interpolated_convex_hull(convex.fpr, convex.tpr)
   register_scale(ProbitScale)
   register_scale(LogOneMinusXScale)
   plt.sca(axs[4])
-  color = plt.scatter(points.fpr, points.tpr, alpha=0.5).get_facecolor()[0]
+  color = plt.scatter(roc.fpr, roc.tpr, alpha=0.5).get_facecolor()[0]
 
   plt.ylim([1e-2, 1 - 1e-2])
   plt.ylabel("True Positive Rate")
@@ -41,22 +34,21 @@ def plot_swets_roc(predicted, labels, axs):
     (norm.cdf(norm.ppf(1-1e-2)-2), 1-1e-2),
     color="lightgray", linestyle = "--", zorder=-10)
   plt.suptitle("Operating Characteristics (Swets SNR Scaling)")
-  fpr, tpr = convex_hull_roc(points.shape[0], points.fpr_literal.values, points.tpr_literal.values)
 
-  jank_fpr, jank_tpr = plot_convex_hull(fpr, tpr, color)
-  equivalent_tpr = project(fpr, tpr, axs)
+  plt.plot(interpolated_smooth.fpr, interpolated_smooth.tpr, linewidth=3, alpha=0.2, color=color)
+  project(convex.fpr, convex.tpr, axs)
 
   plt.sca(axs[3])
-  plt.plot(fpr, tpr, linewidth=3, alpha=0.2, color=color)
-  plt.scatter(points.fpr_literal, points.tpr_literal, s=1)
+  plt.plot(convex.fpr, convex.tpr, linewidth=3, alpha=0.2, color=color)
+  plt.scatter(roc.fpr_literal, roc.tpr_literal, s=1)
   plt.gca().xaxis.set_label_position('top') 
   plt.gca().xaxis.tick_top()
   plt.xlabel('FPR')
   plt.ylabel('TPR')
 
   plt.sca(axs[5])
-  plt.plot(fpr, tpr, linewidth=3, alpha=0.2, color=color)
-  plt.scatter(points.fpr_literal, points.tpr_literal, s=1)
+  plt.plot(convex.fpr, convex.tpr, linewidth=3, alpha=0.2, color=color)
+  plt.scatter(roc.fpr_literal, roc.tpr_literal, s=1)
   plt.gca().xaxis.set_label_position('top') 
   plt.gca().xaxis.tick_top()
   plt.xlabel('FPR')
@@ -64,21 +56,21 @@ def plot_swets_roc(predicted, labels, axs):
   plt.xscale('log')
 
   plt.sca(axs[6])
-  plt.plot(jank_tpr, jank_tpr / (jank_tpr + jank_fpr), linewidth=3, alpha=0.2, color=color)
-  plt.scatter(points.tpr_literal, points.tpr_literal / (points.tpr_literal + points.fpr_literal), s=1)
+  plt.plot(interpolated_smooth.tpr, interpolated_smooth.tpr / (interpolated_smooth.tpr + interpolated_smooth.fpr), linewidth=3, alpha=0.2, color=color)
+  plt.scatter(roc.tpr_literal, roc.tpr_literal / (roc.tpr_literal + roc.fpr_literal), s=1)
   plt.gca().xaxis.set_label_position('top') 
   plt.gca().xaxis.tick_top()
   plt.xlabel('Recall')
   plt.ylabel('Precision')
 
   plt.sca(axs[7])
-  budget = jank_tpr * points.labels.sum() + jank_fpr * (1-points.labels).sum()
-  nne = budget / jank_tpr / points.labels.sum()
-  budget = budget / points.shape[0]
+  budget = interpolated_smooth.tpr * roc.labels.sum() + interpolated_smooth.fpr * (1-roc.labels).sum()
+  nne = budget / interpolated_smooth.tpr / roc.labels.sum()
+  budget = budget / roc.shape[0]
   plt.plot(budget, nne, linewidth=3, alpha=0.2, color=color)
-  budget = points.tpr_literal * points.labels.sum() + points.fpr_literal * (1-points.labels).sum()
-  nne = budget / points.tpr_literal / points.labels.sum()
-  budget = budget / points.shape[0]
+  budget = roc.tpr_literal * roc.labels.sum() + roc.fpr_literal * (1-roc.labels).sum()
+  nne = budget / roc.tpr_literal / roc.labels.sum()
+  budget = budget / roc.shape[0]
   plt.scatter(budget, nne, s=1)
   plt.gca().xaxis.set_label_position('top') 
   plt.gca().xaxis.tick_top()
@@ -86,41 +78,6 @@ def plot_swets_roc(predicted, labels, axs):
   plt.xscale('log')
   plt.xlim([1e-2, 1])
   plt.ylabel('Number Needed to Evaluate')
-  #plt.yscale('log1minusx')
-
-  #plt.sca(axs[7])
-  #pos_like = norm.pdf(norm.ppf(points.tpr_literal))
-  #neg_like = norm.pdf(norm.ppf(points.fpr_literal))
-  #snr = norm.ppf(points.tpr_literal) - norm.ppf(points.fpr_literal)
-  #plt.scatter(neg_like / pos_like, snr, alpha=0.5)
-  #plt.xscale('log')
-  #plt.xlabel('Inv Binormal Like Ratio')
-  #plt.ylabel('SNR\n(Sampled Points)')
-  #axs[7].set_xlim([1e-2, 1e2])
-
-  #plt.sca(axs[6])
-  #pos_like = norm.pdf(norm.ppf(jank_tpr))
-  #neg_like = norm.pdf(norm.ppf(jank_fpr))
-  #snr = norm.ppf(jank_tpr) - norm.ppf(jank_fpr)
-  #plt.scatter(neg_like / pos_like, snr, alpha=0.5)
-  #plt.xscale('log')
-  #plt.xlabel('Inv Binormal Like Ratio')
-  #plt.ylabel('SNR\n(convex hull)')
-  #axs[6].set_xlim(axs[7].get_xlim())
-  #axs[6].set_ylim(axs[7].get_ylim())
-
-  #plt.sca(axs[5])
-  #snr = norm.ppf(points.tpr_literal) - norm.ppf(points.fpr_literal)
-  #plt.plot(points.predicted, snr)
-
-  #plt.sca(axs[6])
-  #pos_like = norm.pdf(norm.ppf(points.tpr_literal))
-  #neg_like = norm.pdf(norm.ppf(points.fpr_literal))
-  #snr = norm.ppf(points.tpr_literal) - norm.ppf(points.fpr_literal)
-  #plt.scatter(neg_like / pos_like, snr, alpha=0.5)
-  #plt.xscale('log')
-  #plt.xlabel('Inv. Binorm. Like. Ratio')
-  #plt.ylabel('SNR\n(Sampled Points)')
 
 def project(fpr, tpr, axs):
   slopes = np.diff(tpr) / np.diff(fpr)
@@ -146,29 +103,9 @@ def project(fpr, tpr, axs):
   plt.sca(axs[2])
   plt.plot(lrt, dy - dy0)
   plt.plot(lrt, 1 - dy0, color="lightgray", linestyle="--", zorder=-10)
-  #plt.plot(lrt, y, linestyle="--", zorder=-10, alpha=0.2)
-  #plt.plot(lrt, yy, color="lightgray", linestyle="--", zorder=-10)
   plt.xscale("log")
   plt.ylabel('Δ Value')
   plt.xlabel('Likelihood Ratio')
-
-  #plt.sca(axs[6])
-  #plt.plot(lrt, x - xx)
-  ##plt.plot(lrt, x, linestyle="--", zorder=-10, alpha=0.2)
-  ##plt.plot(lrt, xx, color="lightgray", linestyle="--", zorder=-10)
-  #plt.xscale("log")
-  #plt.xlabel('Likelihood Ratio')
-  #plt.ylabel('Δ FPR')
-  # Precision doesn't quite work, because it depends separately on the base rate.
-
-  #plt.sca(axs[3])
-  #plt.plot(lrt, (1e-4+tpr[idx]) / (1e-4+fpr[idx]), dashes=[2,2])
-  ##print(tpr[idx] / (1e-4+fpr[idx]))
-  #plt.plot([1, 1e2], [1, 1e2], color="lightgray", zorder=-10, alpha=0.5)
-  #plt.xscale("log")
-  #plt.yscale("log")
-  #plt.xlabel('Likelihood Ratio')
-  #plt.ylabel('Precision (Odds Ratio)')
 
   plt.sca(axs[8])
   plt.plot(lrt, tpr[idx] - fpr[idx])
@@ -176,48 +113,9 @@ def project(fpr, tpr, axs):
   plt.ylabel('Δ Equivalent TP\nwith zero FP')
   plt.xlabel('Likelihood Ratio\n(at Threshold, evaluation uses 1)')
 
-  #plt.sca(axs[7])
-  #i2 = np.searchsorted(slopes, 1)
-  #plt.plot(lrt, np.maximum(0, tpr[i2] - lrt * fpr[i2]) - np.maximum(0, 1 - lrt))
-  #plt.xscale("log")
-  #plt.ylabel('Δ Equivalent TP\nwith zero FP')
-  #plt.xlabel('Likelihood Ratio\n(for evaluation, threshold is 1)')
-
   plt.sca(axs[0])
   color = plt.plot(lrt, fpr[idx])[0].get_color()
   plt.plot(lrt, tpr[idx], color=color, dashes=[1,1])
   plt.legend(["FPR", "TPR"])
   plt.xlabel('Likelihood Ratio')
   plt.xscale("log")
-
-def interpolate(f1, f2, t1, t2):
-  N = 100
-  return np.stack([
-    np.linspace(f1, f2, N), np.linspace(t1, t2, N)
-  ]).T
-
-def convex_hull_roc(_, fpr, tpr):
-  L = 0
-  H = 1
-  fpr = np.concatenate([[L, H, H], fpr])
-  tpr = np.concatenate([[L, L, H], tpr])
-  idx = ConvexHull(
-      np.stack([fpr, tpr]).T,
-      ).vertices
-  idx = np.roll(idx, -1)[1:]
-  fpr = fpr[idx]
-  tpr = tpr[idx]
-  return fpr, tpr
-
-def plot_convex_hull(fpr, tpr, color):
-  fn = iter(fpr)
-  next(fn)
-  tn = iter(tpr)
-  next(tn)
-  points = np.concatenate([
-    interpolate(f1, f2, t1, t2)
-    for f1, f2, t1, t2 in zip(fpr, fn, tpr, tn)
-  ]).T
-  fpr, tpr = points
-  plt.plot(fpr, tpr, linewidth=3, alpha=0.2, color=color)
-  return fpr, tpr
