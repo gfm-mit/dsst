fa3e1fcf36f3dcecf4c4ec2905fdd96d64ddb78c
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-04-28 13:54:50 -0400

    simplify to 3 plots

--- plot/swets.py
+++ plot/swets.py
@@ -3,189 +3,9 @@ import matplotlib.pyplot as plt
 from plot.probit import *
 from plot.log_one_minus import *
 from plot.roc import *
+from plot.plot_library import *
 from sklearn.metrics import roc_auc_score
 from scipy.stats import norm, gaussian_kde
-
-def plot_roc(roc, convex, interpolated_smooth, axs):
-  plt.sca(axs[0])
-  plt.title("ROC")
-  color = plt.scatter(roc.fpr_literal, roc.tpr_literal, s=1).get_facecolor()[0]
-  plt.scatter(convex.fpr[1:-1], convex.tpr[1:-1], s=100, alpha=0.2, color=color)
-  plt.plot(convex.fpr, convex.tpr, linewidth=3, alpha=0.4, color=color)
-  plt.ylabel('TPR')
-  plt.yticks([0, .5, 1], "0% 50% 100%".split())
-  plt.xlabel('FPR')
-  plt.xticks([0, .5, 1], "0% 50% 100%".split())
-  plt.gca().xaxis.set_label_position('top') 
-  plt.gca().xaxis.tick_top()
-
-  #plt.sca(axs[3])
-  #plt.title("ROC (Q-Q plot)")
-  #plt.scatter(roc.fpr_literal, roc.tpr_literal, s=1, alpha=0.7, color=color)
-  #plt.scatter(convex.fpr[1:-1], convex.tpr[1:-1], s=100, alpha=0.2, color=color)
-  #plt.plot(interpolated_smooth.fpr, interpolated_smooth.tpr, linewidth=3, alpha=0.4, color=color)
-
-  #cz0 = 1e-3
-  #z0 = norm.ppf(cz0)
-  #cz1 = 1-1e-3
-  #z1 = norm.ppf(cz1)
-
-  #plt.ylabel("TPR")
-  #plt.yscale('probit')
-  #plt.ylim([cz0, cz1])
-  #plt.yticks([1e-2, 1e-1, 5e-1, 1-1e-1, 1-1e-2], "1% 10% 50% 90% 99%".split())
-
-  #plt.xlabel("FPR")
-  #plt.xscale('probit')
-  #plt.xlim([cz0, cz1])
-  #plt.gca().xaxis.set_label_position('top') 
-  #plt.gca().xaxis.tick_top()
-
-  #plt.axline((cz0, cz0), (cz1, cz1), color="lightgray", linestyle = "--", zorder=-10)
-  #plt.axline((cz0, norm.cdf(z0+1)), (norm.cdf(z1-1), cz1), color="lightgray", linestyle = "--", zorder=-10)
-  #plt.axline((cz0, norm.cdf(z0+2)), (norm.cdf(z1-2), cz1), color="lightgray", linestyle = "--", zorder=-10)
-
-  plt.sca(axs[2])
-  plt.title('ROC (affine transform)')
-  plus = convex.tpr + convex.fpr
-  minus = convex.tpr - convex.fpr
-  plt.plot(-plus, 1+minus, linewidth=3, alpha=0.4, color=color)
-  plt.scatter(-plus[1:-1], 1+minus[1:-1], linewidth=3, s=100, alpha=0.2, color=color)
-  plus = roc.tpr_literal + roc.fpr_literal
-  minus = roc.tpr_literal - roc.fpr_literal
-  plt.scatter(-plus, 1+minus, s=1)
-
-  plt.plot([-0, -1, -2], [1, 2, 1], linestyle='--', color="lightgray", zorder=-10)
-  plt.plot([-0, -2], [1, 1], linestyle=':', color="lightgray", zorder=-10)
-  plt.ylabel('TPR + TNR\n(TPR + 1 - FPR)')
-  plt.yticks([1, 1.5, 2], "100% 150% 200%".split())
-  plt.xlabel('TPR + FPR (backwards)')
-  plt.xticks([-0, -1, -2], "0% 100% 200%".split())
-  plt.gca().set_aspect('equal')
-
-  plt.sca(axs[3])
-  plot_rotated_roc(roc, convex, color)
-
-  plt.sca(axs[1])
-  plot_rotated_roc(roc, convex, color)
-  return color
-
-def plot_rotated_roc(roc, convex, color):
-    plt.title('ROC (rotated)')
-    plus = convex.tpr + convex.fpr
-    minus = convex.tpr - convex.fpr
-    plt.plot(plus, minus, linewidth=3, alpha=0.4, color=color)
-    plt.scatter(plus[1:-1], minus[1:-1], linewidth=3, s=100, alpha=0.2, color=color)
-    plus = roc.tpr_literal + roc.fpr_literal
-    minus = roc.tpr_literal - roc.fpr_literal
-    plt.scatter(plus, minus, s=1)
-
-    plt.plot([0, 1, 2], [0, 1, 0], linestyle='--', color="lightgray", zorder=-10)
-    plt.plot([0, 2], [0, 0], linestyle=':', color="lightgray", zorder=-10)
-    plt.ylabel('TPR - FPR')
-    plt.xlabel('TPR + FPR')
-    plt.xticks([0, 1, 2], "0% 100% 200%".split())
-    plt.yticks([0, .5, 1], "0% 50% 100%".split())
-    plt.gca().set_aspect('equal')
-
-def plot_eta_dollar(eta_density, eta, idx, roc, convex, interpolated_smooth, axs, color):
-  plt.sca(axs[5])
-  plt.title('Convex Conjugate')
-  minus = convex.tpr[idx] - convex.fpr[idx]
-  #plt.plot(eta, minus, linewidth=10, alpha=0.2, color=color)
-  minus[np.roll(minus, 1) == np.roll(minus, -1)] = np.nan
-  plt.plot(eta, 1+minus, linewidth=2, alpha=0.4, color=color)
-  minus = convex.tpr[idx] - convex.fpr[idx]
-  minus[np.roll(minus, 1) != np.roll(minus, -1)] = np.nan
-  plt.plot(eta, 1+minus, linewidth=10, alpha=0.2, color=color)
-  plt.plot(eta, 1+np.ones_like(eta), linestyle='--', color="lightgray", zorder=-10)
-  plt.plot(eta, 1+np.zeros_like(eta), linestyle=':', color="lightgray", zorder=-10)
-  plt.xlabel('η (L.R. Needed for Positive)')
-  plt.xscale('log')
-  plt.xticks([.1, 1, 10], "0.1x 1x 10x".split())
-  plt.ylabel('TPR + TNR')
-  plt.yticks([1, 1.5, 2], "100% 150% 200%".split())
-  
-  plt.sca(axs[6])
-  plt.title('Balanced Accuracy\n(equivalent)')
-  tpr_equiv = convex.tpr[idx] - eta * convex.fpr[idx]
-  tnr_equiv = 1 - convex.fpr[idx] - (1-convex.tpr[idx]) / eta
-  balanced = 1 - (1-tpr_equiv) / (1+eta)
-  plt.plot(eta, balanced)
-  neutral = np.where(eta < 1, 1 / (1+eta), 1 - 1 / (1+eta))
-  plt.plot(eta, np.ones_like(eta), color="lightgray", linestyle='--', zorder=-10)
-  plt.plot(eta, neutral, color="lightgray", linestyle=':', zorder=-10)
-  plt.xlabel('η (L.R. Needed for Positive)')
-  plt.xscale('log')
-  plt.xticks([.1, 1, 10], "0.1x 1x 10x".split())
-  plt.ylabel('Percent of Value')
-  plt.yticks([0, .25, .5, 0.75, 1], "Guessing 25% 50% 75% Oracle".split())
-
-  #plt.sca(axs[10])
-  #plt.title('pure TPR or TNR\n(equivalent)')
-  #flat_idx = tpr_equiv == np.roll(tpr_equiv, 1)
-  #plt.plot(eta[flat_idx], tpr_equiv[flat_idx], linestyle='--', color=color)
-  #plt.plot(eta[~flat_idx], tpr_equiv[~flat_idx], color=color)
-  #flat_idx = tnr_equiv == np.roll(tnr_equiv, -1)
-  #plt.plot(eta[flat_idx], tnr_equiv[flat_idx], linestyle='--', color=color, alpha=0.4)
-  #plt.plot(eta[~flat_idx], tnr_equiv[~flat_idx], color=color, alpha=0.4)
-  #plt.xlabel('Likelihood Ratio')
-  #plt.xscale('log')
-  #plt.ylabel('TPR')
-  #plt.ylim([0, 1])
-  #ax2 = plt.gca().twinx()
-  #ax2.set_ylabel('TNR', color="lightgray")
-  #ax2.tick_params(axis='y', color="lightgray", labelcolor="lightgray")
-
-  #plt.sca(axs[9])
-  #window = (1e-1 < eta) & (eta < 1e1)
-  #plt.plot(eta[window], eta_density[window], color=color, alpha=0.5)
-  #plt.ylabel('Density')
-  #plt.xlabel('Likelihood Ratio')
-  #plt.xscale('log')
-  #plt.yscale('log')
-
-  plt.sca(axs[8])
-  plt.title('Percent of Possible Value')
-  tpr_equiv = convex.tpr[idx] - eta * convex.fpr[idx]
-  tnr_equiv = 1 - convex.fpr[idx] - (1-convex.tpr[idx]) / eta
-  balanced = 1 - (1-tpr_equiv) / (1+eta)
-  neutral = np.where(eta < 1, 1 / (1+eta), 1 - 1 / (1+eta))
-  plt.plot(eta, 1 - (1 - balanced) / (1 - neutral))
-  plt.xlabel('η (L.R. Needed for Positive)')
-  plt.xscale('log')
-  plt.xticks([.1, 1, 10], "0.1x 1x 10x".split())
-  plt.ylabel('Percent of Value')
-  plt.yticks([0, .25, .5, 0.75, 1], "Guessing 25% 50% 75% Oracle".split())
-
-def plot_precision(eta, idx, roc, convex, interpolated_smooth, axs, color):
-  plt.sca(axs[4])
-  plt.plot(interpolated_smooth.tpr, interpolated_smooth.tpr / (interpolated_smooth.tpr + interpolated_smooth.fpr), linewidth=3, alpha=0.2, color=color)
-  plt.scatter(roc.tpr_literal, roc.tpr_literal / (roc.tpr_literal + roc.fpr_literal), s=1)
-  plt.gca().xaxis.set_label_position('top') 
-  plt.gca().xaxis.tick_top()
-  plt.xlabel('Recall')
-  plt.xticks([0, .5, 1], "0% 50% 100%".split())
-  plt.ylabel('Precision')
-  plt.yticks([0, .5, 1], "0% 50% 100%".split())
-
-  plt.sca(axs[7])
-  budget = interpolated_smooth.tpr * roc.labels.sum() + interpolated_smooth.fpr * (1-roc.labels).sum()
-  nne = budget / interpolated_smooth.tpr / roc.labels.sum()
-  budget = budget / roc.shape[0]
-  plt.plot(budget, nne, linewidth=3, alpha=0.2, color=color)
-  budget = roc.tpr_literal * roc.labels.sum() + roc.fpr_literal * (1-roc.labels).sum()
-  nne = budget / roc.tpr_literal / roc.labels.sum()
-  budget = budget / roc.shape[0]
-  plt.scatter(budget, nne, s=1)
-  plt.gca().xaxis.set_label_position('top') 
-  plt.gca().xaxis.tick_top()
-  plt.xlabel('% Predicted Positive\n(Triage Target)')
-  plt.xscale('log')
-  plt.xlim([1e-2, 1])
-  plt.xticks([1e-2, 1e-1, 1], "1% 10% 100%".split())
-  plt.ylabel('NNE (PP / TP)')
-
 
 def plot_9_types(predicted, labels, axs):
   register_scale(ProbitScale)
@@ -193,10 +13,13 @@ def plot_9_types(predicted, labels, axs):
   roc = get_roc(predicted, labels)
   convex = get_roc_convex_hull(roc.shape[0], roc.fpr_literal.values, roc.tpr_literal.values)
   interpolated_smooth = get_roc_interpolated_convex_hull(convex.fpr, convex.tpr)
-  color = plot_roc(roc, convex, interpolated_smooth, axs)
-
   eta_density, eta, idx = get_slopes(convex.fpr, convex.tpr)
-  plot_eta_dollar(eta_density, eta, idx, roc, convex, interpolated_smooth, axs, color)
-  plot_precision(eta, idx, roc, convex, interpolated_smooth, axs, color)
+
+  plt.sca(axs[0])
+  color = plot_roc_swets(roc, convex, interpolated_smooth, axs, color=None)
+  plt.sca(axs[1])
+  plot_eta_percent(eta_density, eta, idx, roc, convex, interpolated_smooth, axs, color, labels.sum() / labels.size)
+  plt.sca(axs[2])
+  plot_precision_at_k(eta, idx, roc, convex, interpolated_smooth, axs, color)
   aucroc = roc_auc_score(labels, predicted)
   print("aucroc", aucroc)
--- scripts/linear.py
+++ scripts/linear.py
@@ -49,7 +49,7 @@ def plot_features(path, axs, weight_ratio=1):
   plot_9_types(y_hat, V.values, axs)
 
 def make_plots():
-  fig, axs = plt.subplots(3, 3, figsize=(12, 8))
+  fig, axs = plt.subplots(1, 3, figsize=(12, 6), width_ratios=[2,1,1])
   plot_features(pathlib.Path("/Users/abe/Desktop/features.csv"), axs, weight_ratio=1e3)
   plot_features(pathlib.Path("/Users/abe/Desktop/features.csv"), axs, weight_ratio=1e-1)
   #plot_features(pathlib.Path("/Users/abe/Desktop/features_graph.csv"), axs)
