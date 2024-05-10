38d0061b941eb37b73aa82a61b0c0ca5485cde48
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-05-10 13:53:07 -0400

    create 2d bitmap dataset type

---
+++ gtorch/datasets/bitmap.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import torch
+import shutil
+from pathlib import Path
+import re
+import matplotlib.pyplot as plt
+import scipy
+from tqdm import tqdm
+import einops
+import pathlib
+
+class SeqDataset(torch.utils.data.Dataset):
+    def __init__(self, metadata, trunc=512):
+        self.trunc = trunc
+        uniq_splits = np.unique(metadata.index)
+        assert uniq_splits.shape[0] == 1, uniq_splits
+        self.md = metadata.copy()
+        rows = []
+        for _, (pkey, coarse) in self.md.iterrows():
+          csv = Path('/Users/abe/Desktop/BITMAP/') / f"{pkey}.csv"
+          data = np.load(csv)
+          print(data.head().T)
+          assert False
+          data = np.nan_to_num(data, 0)
+          data = pd.DataFrame(data, columns="symbol task box t v_mag2 a_mag2 dv_mag2 cw j_mag2".split())
+          data = data.groupby("symbol task box".split()).apply(lambda g: g.mean(skipna=True))
+          data = data.reset_index(drop=True).drop(columns="symbol task box".split())
+          data["pkey"] = pkey
+          data["label"] = coarse
+          data = data.set_index("pkey")
+          rows += [data]
+        assert len(rows)
+        self.files = pd.concat(rows, axis=0)
+
+    def __getitem__(self, index):
+        coarse = self.files.iloc[index, -1]
+        nda = self.files.iloc[index, :-1].astype(float).values[:, np.newaxis]
+        x = torch.Tensor(nda[:, :])
+        y = torch.LongTensor([coarse])
+        return x, y
+
+    def __len__(self):
+        return self.files.shape[0]
+
+def get_loaders():
+  labels = pd.read_csv(pathlib.Path("/Users/abe/Desktop/meta.csv")).set_index("AnonymizedID")
+  #assert(features.index.difference(labels.index).empty), (features.index, labels.index)
+  labels = labels.Diagnosis[labels.Diagnosis.isin(["Healthy Control", "Dementia-AD senile onset"])] == "Dementia-AD senile onset"
+  labels = labels.reset_index()
+  split = labels.index.astype(str).map(hash).astype(np.uint64) % 5
+  labels["split"] = np.where(split == 0, 'test', np.where(split == 1, 'validation', 'train'))
+  labels = labels.set_index("split")
+  train_data = SeqDataset(labels.loc["train"])
+  #val_data = SeqDataset(metadata.loc["val"])
+  #test_data = SeqDataset(metadata.loc["test"])
+  train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+  #val_loader = torch.utils.data.DataLoader(
+  #    val_data, batch_size=1000, shuffle=True, collate_fn=collate_fn_padd)
+  #test_loader = torch.utils.data.DataLoader(
+  #    test_data, batch_size=1000, shuffle=True, collate_fn=collate_fn_padd)
+  return train_loader
---
+++ scripts/bitmap.py
@@ -0,0 +1,54 @@
+import random
+from lxml import etree
+import pandas as pd
+import pathlib
+import matplotlib.pyplot as plt
+from skimage.draw import line
+from scipy.sparse import csr_matrix, save_npz, vstack
+import re
+
+from etl.parse_semantics import *
+from etl.parse_dynamics import *
+
+def rasterize_path_to_bitmap(points, bitmap=None):
+  for i in range(len(points) - 1):
+      x1, y1 = points[i]
+      x2, y2 = points[i + 1]
+      rr, cc = line(y1, x1, y2, x2)
+      good = (rr < bitmap.shape[0]) & (cc < bitmap.shape[1])
+      rr, cc = rr[good], cc[good]
+      bitmap[rr, cc] = 1
+  return bitmap
+
+if __name__ == "__main__":
+  assigned_path = pathlib.Path("/Users/abe/Desktop/ASSIGNED/")
+  bitmap_path = pathlib.Path("/Users/abe/Desktop/BITMAP/")
+  bitmap_path.mkdir(parents=True, exist_ok=True)
+  files = list(assigned_path.glob("*.csv"))
+  random.shuffle(files)
+  semantics = load_semantics()
+  W = 64
+  for csv in files:
+    pkey = next(re.finditer(r"/(CIN\d+).csv$", str(csv))).group(1)
+    df = pd.read_csv(csv)
+    #print("df.head().T", df.head().T)
+    df = assign_semantics(df, semantics)
+    meta = []
+    data = []
+    for (symbol, task, box), g in df.groupby("symbol task box".split()):
+      bitmap = np.zeros([W, W], dtype=np.uint8)
+      if symbol != 12:
+        continue
+      for _, stroke in g.groupby("stroke_id"):
+        xy = stroke["x y".split()].values
+        xy = np.round(W * xy).astype(int)
+        xy[:, 1] = W/4 - xy[:, 1]
+        rasterize_path_to_bitmap(xy, bitmap)
+      bitmap = csr_matrix(bitmap.reshape([-1]))
+      meta += [(symbol, task, box)]
+      data += [bitmap]
+    data = vstack(data)
+    meta = pd.DataFrame(meta, columns="symbol task box".split())
+    meta.to_csv(bitmap_path / f"{pkey}.csv")
+    save_npz(bitmap_path / f"{pkey}.npz", data)
+    print(pkey)
