2f2dd375f48a41efddda2c41ef22c3e23479eb3b
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-05-18 13:52:59 -0400

    add some commented-out gradient debugging code to optimize.py

--- gtorch/optimize/optimize.py
+++ gtorch/optimize/optimize.py
@@ -24,6 +24,20 @@ def optimize(epoch, model, optimizer, train_loader):
   DEVICE = next(model.parameters()).device
   model.train()
   loader_has_batches = False
+  #gradients = {}
+  #
+  #def hook(module, grad_input, grad_output):
+  #    print(type(module), grad_input[0].shape, grad_output[0].shape)
+  #    gradients[module] = torch.cat([
+  #       grad_input[0],
+  #       #torch.ones(grad_input[0].shape[0], 1).to(DEVICE),
+  #       grad_output[0],
+  #    ], axis=1)
+  #    gradients[str(module) + "_in"] = grad_input[0].shape
+  #    gradients[str(module) + "_out"] = grad_output[0].shape
+  #
+  #for layer in model.children():
+  #  layer.register_backward_hook(hook)
   for data, target in train_loader:
     loader_has_batches = True
     optimizer.zero_grad()
@@ -33,6 +47,12 @@ def optimize(epoch, model, optimizer, train_loader):
     #    output, target.to(DEVICE), weight=class_weights.to(DEVICE))
     loss = torch.nn.functional.nll_loss(output, target[:, 0].to(DEVICE))
     loss.backward()
+    #print("target", target[0])
+    #for k, v in gradients.items():
+    #  print(k, v)
+    #print("output.grad", output.grad)
+    #for k, v in model.named_parameters():
+    #  print(k, v.grad)
     optimizer.step()
   assert loader_has_batches
   torch.save(model.state_dict(), './results/model.pth')
