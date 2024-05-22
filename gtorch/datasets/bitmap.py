22abb19c4ec22545e21f3336c8abe4f4086a5461
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-05-22 18:31:19 -0400

    speedup bitmap loader

--- gtorch/datasets/bitmap.py
+++ gtorch/datasets/bitmap.py
@@ -16,7 +16,9 @@ class SeqDataset(torch.utils.data.Dataset):
         uniq_splits = np.unique(metadata.index)
         assert uniq_splits.shape[0] == 1, uniq_splits
         self.md = metadata.copy()
-        rows = []
+        features = []
+        labels = []
+        groups = []
         for _, (pkey, coarse) in self.md.iterrows():
           csv = Path('/Users/abe/Desktop/BITMAP/') / f"{pkey}.csv"
           npz = Path('/Users/abe/Desktop/BITMAP/') / f"{pkey}.npz"
@@ -25,30 +27,30 @@ class SeqDataset(torch.utils.data.Dataset):
           N = np.sqrt(data.shape[1])
           assert N == int(N)
           N = int(N)
-          meta["bitmap"] = [
+          features += [
              data[i].reshape([N, N])
              for i in range(data.shape[0])
           ]
-          meta["pkey"] = pkey
-          meta["label"] = coarse
-          meta = meta.set_index("pkey")
-          #meta = meta[meta.task.isin([2, 3])]
-          rows += [meta]
-        assert len(rows)
-        self.files = pd.concat(rows, axis=0)
+          labels += [coarse] * data.shape[0]
+          groups += [pkey] * data.shape[0]
+        assert len(features)
+        self.features = np.array(features)
+        self.labels = np.array(labels)
+        self.groups = np.array(groups)
 
     def __getitem__(self, index):
-        coarse = self.files.iloc[index, -1]
-        nda = self.files.iloc[index, -2].toarray()
+        coarse = self.labels[[index]]
+        nda = self.features[index].toarray()
         x = torch.Tensor(nda[np.newaxis, :, :])
-        y = torch.LongTensor([coarse])
+        y = torch.LongTensor(coarse)
         if self.test_split:
-          return x, y, index
+          g = self.groups[index]
+          return x, y, g
         else:
-          return x, y
+           return x, y
 
     def __len__(self):
-        return self.files.shape[0]
+        return self.labels.shape[0]
 
 def get_loaders():
   labels = pd.read_csv(pathlib.Path("/Users/abe/Desktop/meta.csv")).set_index("AnonymizedID")
