6b82c91e66c86b0eb8f98ef6b0f62c35711ffcab
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-06 12:13:02 -0400

    Create cnn_1d_atrous.py
    supposedly might be better than the normal tower.  jury's still out if you ask me

---
+++ gtorch/models/cnn_1d_atrous.py
@@ -0,0 +1,99 @@
+#@title def get_jank_model(Linear):
+import numpy as np
+import pandas as pd
+import torch
+from einops.layers.torch import Rearrange
+
+import gtorch.models.base
+
+class OneCat(torch.nn.Module):
+  def forward(self, input):
+    return torch.cat([torch.zeros([input.shape[0], 1]), input], axis=1)
+
+class PrintCat(torch.nn.Module):
+  def forward(self, input):
+    print(input.shape)
+    return input
+
+class Cnn(gtorch.models.base.Base):
+  def __init__(self, n_features=12, n_classes=2, device='cpu'):
+    self.classes = n_classes
+    self.features = n_features
+    self.device = device
+    super().__init__()
+
+  def get_architecture(self, hidden_width='unused'):
+    model = torch.nn.Sequential(
+        # b n c
+        Rearrange('b n c -> b c n'),
+        torch.nn.Conv1d(
+          12,
+          self.features,
+          kernel_size=5,
+          stride=5),
+        torch.nn.BatchNorm1d(num_features=self.features),
+        torch.nn.Conv1d(
+          self.features,
+          self.features,
+          kernel_size=5,
+          dilation=4),
+        torch.nn.BatchNorm1d(num_features=self.features),
+        torch.nn.Conv1d(
+          self.features,
+          self.features,
+          kernel_size=5,
+          padding=32,
+          dilation=16),
+        torch.nn.BatchNorm1d(num_features=self.features),
+        torch.nn.AdaptiveMaxPool1d(1),
+        Rearrange('b c 1 -> b c'),
+        torch.nn.BatchNorm1d(num_features=12),
+        torch.nn.Linear(self.features, self.classes),
+        torch.nn.LogSoftmax(dim=-1),
+    )
+    model = model.to(self.device)
+    return model
+
+  def get_parameters(self):
+    return dict(
+      #solver='adam',
+      schedule='onecycle',
+      weight_decay=0,
+      momentum=0,
+      beta2=0.9,
+      pct_start=0.0,
+
+      max_epochs=10,
+      min_epochs=0,
+
+      learning_rate=1e-1, # stupid edge of stability!!
+      hidden_width=2,
+    )
+
+  def get_tuning_ranges(self):
+    return dict(
+        #nonce=np.arange(5),
+        #learning_rate=np.geomspace(1e-5, 1e-0, 15),
+        #weight_decay=np.geomspace(1e-8, 1e-4, 15),
+        #pct_start=np.geomspace(0.01, .95, 15),
+        #max_epochs=np.geomspace(5, 100, 15).astype(int),
+        #momentum=1-np.geomspace(.1, 1e-5, 15),
+        #beta2=1-np.geomspace(.5, .0001, 15),
+    )
+
+  def get_coefficients(self, model):
+    if self.classes == 2:
+      return pd.Series(
+        np.concatenate([
+          [model.state_dict()['2.bias'].numpy()[1]
+          - model.state_dict()['2.bias'].numpy()[0]],
+          model.state_dict()['2.weight'].numpy()[1]
+          - model.state_dict()['2.weight'].numpy()[0]
+        ])
+      )
+    return pd.Series(
+      np.concatenate([
+        model.state_dict()['1.bias'].numpy(),
+        model.state_dict()['1.weight'].numpy().flatten(),
+      ])
+    )
