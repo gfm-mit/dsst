2d0b6ebfc780b8dc2de8b8b84df72776ed2395da
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-10 17:35:55 -0400

    clean up lr_finder code
    add tqdm, mostly

--- gtorch/hyper/lr_finder.py
+++ gtorch/hyper/lr_finder.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import itertools
+from tqdm import tqdm
 
 import gtorch.models.base
 from gtorch.optimize.loss import classify, next_token
@@ -32,15 +33,14 @@ def find_lr(params, model_factory_fn, train_loader=None, pretraining=None):
         layer.reset_parameters()
 
   new_params = dict(**params)
-  low, high, N = 3.0, 30.0, 15
+  low, high, N = 1e-2, 1e2, 30
   new_params["schedule"] = "exponential"
   new_params["learning_rate"] = low
   new_params["schedule_gamma"] = np.power(high / low, 1.0 / N)
-  print(new_params["schedule_gamma"])
   optimizer, scheduler = get_optimizer(new_params, model)
   lrs = []
   losses = []
-  for e, batch in zip(range(N), itertools.cycle(train_loader)):
+  for e, batch in zip(tqdm(range(N)), itertools.cycle(train_loader)):
     losses += [loss_fn(0, model, optimizer, [batch])]
     lrs += [scheduler.get_last_lr()[0]]
     scheduler.step()
