4344031abb430cc6f9e22d3556982b561fe5d56a
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-11 09:51:38 -0400

    Update lr_finder.py

--- plot/lr_finder.py
+++ plot/lr_finder.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import scipy
 
-def plot_lr(lrs, losses, smooth=3):
+def plot_lr(lrs, losses, smooth=3, ax=None):
   # pretty sure these two operators commute
   losses = scipy.ndimage.gaussian_filter1d(losses, smooth / 2, mode='nearest')
-  deltas = np.diff(losses)
-  thresh = np.nanmedian(deltas[deltas > 0]) - np.nanmedian(deltas[deltas < 0])
-  pd.Series(deltas, index=lrs[1:]).plot()
-  plt.yscale('symlog', linthresh=thresh)
+  losses = pd.Series(losses, index=lrs)
+  losses.plot(ax=ax)
   plt.xscale('log')
-  plt.axhline(0, color='lightgray', zorder=-10, linestyle='--')
-  plt.show()
+  plt.yscale('log')
+  return losses
