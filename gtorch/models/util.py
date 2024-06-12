28ba4bb70ddb56d41a8ebecf0248dc77086b0e00
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-12 14:55:30 -0400

    remove FFT transformer
    was based on a misunderstanding - it's not and really cannot be causal

--- gtorch/models/util.py
+++ gtorch/models/util.py
@@ -19,16 +19,6 @@ class PadCat(torch.nn.Module):
     n_h = int(np.ceil(input.shape[2] / self.width)) * self.width
     return torch.nn.functional.pad(input, (0, n_h - input.shape[2]))
 
-class FourierAttention(torch.nn.Module):
-    batch_first = False
-
-    def forward(self, query, key, value,
-                attn_mask=None,
-                key_padding_mask=None,
-                is_causal=None,
-                need_weights=False):
-        return torch.fft.fft(torch.fft.fft(query, axis=0), axis=2).real
-
 class NoopAttention(torch.nn.Module):
     def forward(self, query, key, value,
                 attn_mask=None,
