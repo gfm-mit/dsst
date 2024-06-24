331b46f10d15116765a8dfd196a1c3f25450ce50
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-24 11:32:54 -0400

    params; better settings for CNN 1d

--- models/cnn_1d.py
+++ models/cnn_1d.py
@@ -2,6 +2,7 @@ import torch
 from einops.layers.torch import Rearrange
 
 import models.base
+import models.util
 
 
 class Cnn(models.base.Base):
@@ -10,27 +11,40 @@ class Cnn(models.base.Base):
     n_features = n_features
     super().__init__(device=device)
 
-  def get_classifier_architecture(self, n_features=12):
+  def get_classifier_architecture(self, n_features=None, kernel_split=None, activation=None):
+    kernels = [int(k) for k in kernel_split.split(',')]
+    assert len(kernels) == 3
+    if activation == "relu":
+      activation = torch.nn.ReLU
+    elif activation == "gelu":
+      activation = torch.nn.GELU
+    elif activation == "none":
+      activation = torch.nn.Identity
+    elif activation == "swish":
+      activation = torch.nn.SiLU
     model = torch.nn.Sequential(
         # b n c
         Rearrange('b n c -> b c n'),
         torch.nn.Conv1d(
           12,
           n_features,
-          kernel_size=4,
-          stride=4),
+          kernel_size=kernels[0],
+          stride=kernels[0]),
+        activation(),
         torch.nn.BatchNorm1d(num_features=n_features),
         torch.nn.Conv1d(
           n_features,
           n_features,
-          kernel_size=4,
-          stride=4),
+          kernel_size=kernels[1],
+          stride=kernels[1]),
+        activation(),
         torch.nn.BatchNorm1d(num_features=n_features),
         torch.nn.Conv1d(
           n_features,
           n_features,
-          kernel_size=4,
-          stride=4),
+          kernel_size=kernels[2],
+          stride=kernels[2]),
+        activation(),
         torch.nn.BatchNorm1d(num_features=n_features),
         #PrintfModule('After 64x down'),
         torch.nn.AdaptiveMaxPool1d(1),
@@ -47,13 +61,15 @@ class Cnn(models.base.Base):
       scheduler='warmup',
       optimizer='samadam',
       weight_decay=0,
-      momentum=0,
+      momentum=0.9,
       conditioning_smoother=0.999,
       warmup_epochs=5,
 
-      max_epochs=80,
+      max_epochs=50,
 
-      learning_rate=1.5e-2,
+      learning_rate=1e-2,
 
-      n_features=48,
+      n_features=32,
+      kernel_split='2,2,16',
+      activation="swish",
     )
