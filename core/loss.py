2660e4b5f331c4ed143d6625a45be93f34bbf5ca
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-24 20:15:15 -0400

    feature: report D when using prodigy
    convenient LR tuning properties are convenient

--- core/loss.py
+++ core/loss.py
@@ -10,14 +10,16 @@ def get_task_loss(model, optimizer, train_loader, task):
     else:
       loss = next_token(model, optimizer, train_loader)
     description = 'Last Batch MSE={:.2f}'.format(np.sqrt(loss))
-    return loss, description
   else:
     if optimizer.__module__ == "pytorch_optimizer.optimizer.sam":
       loss = core.loss_sam.classify(model, optimizer, train_loader)
     else:
       loss = classify(model, optimizer, train_loader)
     description = 'Last Batch Perplexity={:.2f}'.format(np.exp(loss))
-    return loss, description
+  if optimizer.__module__ == "pytorch_optimizer.optimizer.prodigy":
+    loss = optimizer.param_groups[0]["d"]
+    description = 'D={:.2e}  {}'.format(loss, description)
+  return loss, description
 
 def balance_class_weights(target):
     w = torch.nn.functional.one_hot(target).float().mean(axis=0)
