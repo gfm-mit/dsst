a63e1e3b1e4e6c5b549260c3bf308316a924c0c4
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-25 23:36:35 -0400

    params: fft cnn sometimes as high as 85%

--- models/cnn_1d_fft.py
+++ models/cnn_1d_fft.py
@@ -28,16 +28,19 @@ class Cnn(models.base.Base):
     return layers
 
   def get_classifier_architecture(
-      self, arch_kernel=None, arch_fft_width=None, arch_conv_width=None, arch_fft_length=None, **kwargs):
+      self, arch_kernel=None, arch_fft_width=None, arch_conv_width=None, arch_fft_length=None, arch_dropout=None, **kwargs):
     model = torch.nn.Sequential(
         # b n c
         Rearrange('b n c -> b c n'),
         torch.nn.Conv1d(12, arch_fft_width, kernel_size=arch_kernel),
         torch.nn.SiLU(),
         models.util.FftBlock(trim=arch_fft_length),
-        torch.nn.Dropout1d(0.1),
+        torch.nn.Dropout1d(arch_dropout),
         torch.nn.BatchNorm1d(num_features=arch_fft_width),
         torch.nn.Conv1d(arch_fft_width, arch_conv_width, 1),
+        torch.nn.SiLU(),
+        torch.nn.BatchNorm1d(num_features=arch_conv_width),
+        torch.nn.Conv1d(arch_conv_width, arch_conv_width, 1),
         torch.nn.SiLU(),
         Rearrange('b c n -> b (c n)'),
         torch.nn.BatchNorm1d(num_features=arch_conv_width * arch_fft_length),
@@ -49,8 +52,8 @@ class Cnn(models.base.Base):
 
   def get_parameters(self, **kwargs):
     return dict(
-      scheduler='warmup',
-      optimizer='samadam',
+      scheduler='none',
+      optimizer='prodigy',
       weight_decay=0,
       momentum=0.9,
       conditioning_smoother=0.999,
@@ -62,4 +65,5 @@ class Cnn(models.base.Base):
       arch_fft_length=32,
       arch_fft_width=192,
       arch_conv_width=12,
+      arch_dropout=0.1,
     )
