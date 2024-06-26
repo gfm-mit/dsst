8b45eefca4641eabc65a4402236dacd2493a8457
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-26 18:42:07 -0400

    arch: transformer runs now
    doesn't work, but at least it runs

--- models/transformer.py
+++ models/transformer.py
@@ -1,110 +1,103 @@
 import numpy as np
 import torch
+from einops import rearrange
+from einops.layers.torch import Rearrange
+import re
 
 import models.base
 from models.util import NoopAttention
 
 
 class Decoder(torch.nn.Module):
-  def __init__(self, n_features, device=None, causal=False):
+  def __init__(self, arch_width, arch_ff_width, arch_depth, arch_head, arch_dropout, causal=False):
       super(Decoder, self).__init__()
       decoder_layer = torch.nn.TransformerDecoderLayer(
-        d_model=12,
-        nhead=2,
-        dim_feedforward=n_features,
+        d_model=arch_width,
+        nhead=arch_head,
+        dim_feedforward=arch_ff_width,
         batch_first=True,
       )
       decoder_layer.self_attn = torch.nn.MultiheadAttention(
-        embed_dim=12,
-        num_heads=2,
-        dropout=0.1,
+        embed_dim=arch_width,
+        num_heads=arch_head,
+        dropout=arch_dropout,
         batch_first=True,
         bias=True)
       decoder_layer.multihead_attn = NoopAttention() # this is used on the memory
-      self.decoder = torch.nn.TransformerDecoder(decoder_layer=decoder_layer, num_layers=2)
+      self.decoder = torch.nn.TransformerDecoder(decoder_layer=decoder_layer, num_layers=arch_depth)
       self.causal = causal
-      self.device = device
 
   def forward(self, input):
     mask = None
     if self.causal:
       seq_len = input.shape[1]
       # tgt, src
-      mask = torch.Tensor(np.tril(np.ones((seq_len, seq_len)), k=-1).astype(bool)).to(self.device)
+      mask = torch.Tensor(np.tril(np.ones((seq_len, seq_len)), k=-1).astype(bool)).to(input.device)
     return self.decoder(input, memory=None, tgt_mask=mask)
 
-class GetClassifierOutputs(torch.nn.Module):
-  def __init__(self, kind=None):
-    self.kind = kind
-    assert self.kind in "meanmax max mean last".split()
-    super(GetClassifierOutputs, self).__init__()
-
-  def forward(self, input):
-    if self.kind == "max":
-      return torch.amax(input, dim=1)
-    elif self.kind == "mean":
-      nonzeros = torch.sum(torch.amax(input != 0, dim=2, keepdim=True), dim=1)
-      return torch.sum(input, dim=1) / nonzeros
-    elif self.kind == "meanmax":
-      max = torch.amax(input, dim=1)
-      nonzeros = torch.sum(torch.amax(input != 0, dim=2, keepdim=True), dim=1)
-      mean = torch.sum(input, dim=1) / nonzeros
-      return torch.cat([max, mean], dim=1)
-    elif self.kind == "last":
-      return input[:, -1, :]
-    assert False, "unknown transformer aggregation kind: {}".format(self.kind)
-
 class Transformer(models.base.SequenceBase):
-  def __init__(self, n_layers=2, n_features=12, n_classes=2, device='cpu'):
+  def __init__(self, n_classes=2, n_inputs=12, device='cpu'):
     self.classes = n_classes
-    self.features = n_features
-    self.layers = n_layers
+    self.inputs = n_inputs
     super().__init__(device=device)
 
-  def get_next_token_architecture(self):
+  def get_next_token_architecture(self, **kwargs):
     model = torch.nn.Sequential(
         # b n c
-        Decoder(n_features=12, device=self.device, causal=True),
+        Decoder(causal=True, **kwargs), # TODO: try False
+        torch.nn.SiLU(),
+        torch.nn.LayerNorm(normalized_shape=kwargs['arch_width']),
+        torch.nn.Linear(kwargs['arch_width'], self.inputs),
     )
     model = model.to(self.device)
     return model
+  
+  def translate_state_dict(self, next_token_state_dict):
+    classifier_state_dict = {}
+    for k, v in next_token_state_dict.items():
+      if re.match("0[.](weight|bias)_[ih][ih]_l\d", k):
+        kk = k.replace("1.residual.", "") # unused
+        print(f"saving param {k=} {kk=}")
+        classifier_state_dict[kk] = v
+    return classifier_state_dict
 
-  def get_classifier_architecture(self, agg_kind=None):
-    n_features = self.features
-    if agg_kind == "meanmax":
-      n_features *= 2
+  def get_classifier_architecture(self, **kwargs):
     model = torch.nn.Sequential(
         # b n c
-        Decoder(n_features=12, device=self.device, causal=True), # TODO: try False
-        GetClassifierOutputs(kind=agg_kind),
-        torch.nn.BatchNorm1d(num_features=n_features),
-        torch.nn.Linear(n_features, self.classes),
+        Decoder(causal=True, **kwargs), # TODO: try False
+        Rearrange('b n c -> b c n'),
+        torch.nn.AdaptiveMaxPool1d(1),
+        Rearrange('b c 1 -> b c'),
+        # TODO: why does this work better than adding more layers?  and why is taking the last hidden state not enough?
+        torch.nn.Linear(kwargs['arch_width'], self.classes),
         torch.nn.LogSoftmax(dim=-1),
     )
     model = model.to(self.device)
     return model
 
-  def get_next_token_parameters(self, **kwargs):
+  def get_next_token_parameters(self):
     return dict(
-      scheduler='none',
-      optimizer='sfadam',
-      learning_rate=2e-2,
-      momentum=0.9,
-      warmup_step=3,
-      max_epochs=200,
+      scheduler='warmup',
+      optimizer='samadam',
+      warmup_epochs=2,
+      max_epochs=10,
+      learning_rate=3e-2,
     )
 
   def get_classifier_parameters(self, **kwargs):
     return dict(
-      scheduler='none',
+      scheduler='warmup', # TODO: try this with a scheduler
       optimizer='samadam',
       weight_decay=0,
-      momentum=.9,
-      conditioning_smoother=.999,
-      agg_kind="max",
+      momentum=0.9,
+      conditioning_smoother=0.999,
+      warmup_epochs=2,
+      max_epochs=50,
+      learning_rate=3e-3,
 
-      max_epochs=5,
-      warmup_epochs=5,
-
-      learning_rate=1.3e-1, # stupid edge of stability!!
+      arch_depth=2,
+      arch_width=12,
+      arch_ff_width=12,
+      arch_dropout=0.1,
+      arch_head=2,
     )
