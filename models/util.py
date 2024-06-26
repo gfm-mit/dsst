0893a787571ab2171077172e0a525d6294ff3718
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-26 14:03:28 -0400

    feature: Sine block

--- models/util.py
+++ models/util.py
@@ -81,3 +81,11 @@ class ResidualBlock(torch.nn.Module):
 
     def forward(self, inputs):
         return self.residual(inputs) + inputs
+
+class Sine(torch.nn.Module):
+  def __init__(self, scale=1):
+    super().__init__()
+    self.scale = scale
+
+  def forward(self, x):
+    return torch.sin(self.scale*x)
