60c1223bf99588314d260998faa6d538773cc186
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-06-28 17:05:01 -0400

    arch: try a fancier regression head on transformer
    doesn't work, though

--- models/transformer.py
+++ models/transformer.py
@@ -8,7 +8,8 @@ from models.util import NoopAttention
 
 
 class Decoder(torch.nn.Module):
-  def __init__(self, arch_width, arch_ff_width, arch_depth, arch_head, arch_dropout, causal=False):
+  def __init__(self, arch_width, arch_ff_width, arch_depth, arch_head, arch_dropout,
+               arch_token_resid_width='unused', arch_token_resid_depth='unused', causal=False):
       super(Decoder, self).__init__()
       decoder_layer = torch.nn.TransformerDecoderLayer(
         d_model=arch_width,
@@ -35,6 +36,36 @@ class Decoder(torch.nn.Module):
       mask = torch.Tensor(np.tril(np.ones((seq_len, seq_len)), k=-1).astype(bool)).to(input.device)
     #print(f"{mask.shape=}")
     return self.decoder(input, memory=None, tgt_mask=mask)
+  
+class TokenResid(torch.nn.Module):
+    def __init__(self, arch_token_resid_width, arch_token_resid_depth, arch_width):
+      self.arch_token_resid_width = arch_token_resid_width
+      self.arch_token_resid_depth = arch_token_resid_depth
+      self.arch_width = arch_width
+      self.stack = self.get_outer_stack()
+    
+    def get_inner_stack(self):
+      return models.util.ResidualBlock(torch.nn.Sequential(
+        torch.nn.LayerNorm(normalized_shape=self.arch_token_resid_width),
+        torch.nn.Linear(self.arch_token_resid_width, self.arch_token_resid_width),
+        torch.nn.SiLU()))
+    
+    def get_outer_stack(self):
+      return models.util.ResidualBlock(torch.nn.Sequential(
+        torch.nn.SiLU(),
+        torch.nn.LayerNorm(normalized_shape=self.arch_width),
+        torch.nn.Linear(self.arch_width, self.arch_token_resid_width),
+        torch.nn.SiLU(),
+        *[self.get_inner_stack() for _ in range(self.arch_token_resid_depth)],
+        torch.nn.LayerNorm(normalized_shape=self.arch_token_resid_width),
+        torch.nn.Linear(self.arch_token_resid_width, self.arch_width),
+        torch.nn.SiLU(),
+      ))
+    
+    def forward(self, input):
+      if self.arch_token_resid_width == 0:
+        return input
+      return self.stack(input)
 
 class Transformer(models.base.SequenceBase):
   def __init__(self, n_classes=2, n_inputs=12, device='cpu'):
@@ -48,6 +79,11 @@ class Transformer(models.base.SequenceBase):
         models.util.SineProjection(self.inputs, kwargs['arch_width'], scale=1, axis=-1), # why is scale 1?
         Decoder(causal=True, **kwargs), # TODO: try False
         torch.nn.SiLU(),
+        TokenResid(
+          arch_token_resid_depth=kwargs['arch_token_resid_depth'],
+          arch_token_resid_width=kwargs['arch_token_resid_width'],
+          arch_width=kwargs['arch_width']
+          ) if kwargs['arch_token_resid_width'] > 0 else torch.nn.Identity(),
         torch.nn.LayerNorm(normalized_shape=kwargs['arch_width']),
         torch.nn.Linear(kwargs['arch_width'], self.inputs),
     )
@@ -82,13 +118,14 @@ class Transformer(models.base.SequenceBase):
     return dict(
       scheduler='warmup',
       optimizer='samadam',
-      warmup_epochs=2,
-      max_epochs=10,
-      learning_rate=1e-2,
+      warmup_epochs=5,
+      max_epochs=15,
+      learning_rate=2e-2,
     )
 
   def get_classifier_parameters(self, **kwargs):
     return dict(
+      # this gets like 84% AUC, surprisingly enough
       scheduler='warmup', # TODO: try this with a scheduler
       optimizer='samadam',
       weight_decay=0,
@@ -103,4 +140,7 @@ class Transformer(models.base.SequenceBase):
       arch_ff_width=12,
       arch_dropout=0.05,
       arch_head=4,
+
+      arch_token_resid_width=0,
+      arch_token_resid_depth=1,
     )
