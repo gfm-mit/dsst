72c7908f113083c3f95500a6062e4d15fabc3d33
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-07-01 19:55:37 -0400

    Unified tuning config in launcher.py
    Also sadly prune some unused branches

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -3,8 +3,14 @@ import pathlib
 import pstats
 import sys
 import cProfile
+
+import pandas as pd
+import scipy
+import sklearn
+import models.cnn_2d
+import models.linear_bnc
+from util.config import TomlAction
 import util.excepthook
-import tomli
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,22 +23,9 @@ import wrappers.experiment
 import wrappers.tune
 import plot.lr_finder
 import plot.tune
+import plot.history
 import models.registry
-
-class TomlAction(argparse.Action):
-    def __init__(self, toml_key=None, **kwargs):
-        self.key = toml_key
-        assert "default" not in kwargs
-        kwargs["default"] = {}
-        super(TomlAction, self).__init__(**kwargs)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-      assert isinstance(values, str)
-      with open(values, 'rb') as stream:
-        config = tomli.load(stream)
-      if self.key is not None:
-        config = config[self.key]
-      setattr(namespace, self.dest, config)
+import util.config
 
 class LogAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -42,12 +35,7 @@ class LogAction(argparse.Action):
 
 def parse_args():
   parser = argparse.ArgumentParser(description='Run a linear pytorch model')
-  group = parser.add_mutually_exclusive_group()
-  group.add_argument('--tune', action=TomlAction, toml_key='tune', help='Parameter ranges to tune')
-  group.add_argument('--compare', action=TomlAction, toml_key='compare', help='Run multiple prespecified configs')
-  group.add_argument('--find_lr', action=TomlAction, toml_key='find_lr', help='Config file for CLR method to find learning rate')
-  group.add_argument('--config', action=TomlAction, help='read a config toml file')
-  group.add_argument('--test', action='store_true', help='Train one batch on each model class')
+  parser.add_argument('--config', action=TomlAction, help='read a config toml file')
 
   parser.add_argument('--coef', action='store_true', help='Plot coefficients')
   parser.add_argument('--profile', action='store_true', help='Profile training')
@@ -56,7 +44,7 @@ def parse_args():
   parser.add_argument('--device', default='cpu', help='torch device')
   parser.add_argument('--model', default='linear', help='which model class to use')
   parser.add_argument('--task', default='classify', choices=set("next_token classify classify_patient classify_section".split()), help='training target / loss')
-  parser.add_argument('--history', default='none', choices=set("none train val".split()), help='Plot history of loss')
+  parser.add_argument('--history', default='none', choices=set("none train val variance".split()), help='Plot history of loss')
 
   parser.add_argument('--disk', default='none', choices=set("none load save freeze".split()), help='whether to persist the model (or use persisted)')
   parser.add_argument('--log', action=LogAction, default='', help='filename to log metrics and parameters')
@@ -67,39 +55,26 @@ def parse_args():
 def main():
   sys.excepthook = util.excepthook.custom_excepthook
   # will save stack traces from creation in components, makes error messages less stupid
-  #torch.autograd.set_detect_anomaly(True)
+  import torch
+  torch.autograd.set_detect_anomaly(True)
 
   args = parse_args()
 
   if args.bitmap:
+    args.model = "2d"
     BUILDER = models.cnn_2d.Cnn
-    train_loader, val_loader, test_loader = etl.torch.bitmap.get_loaders()
+    train_loader, val_loader, calibration_loader, test_loader = etl.torch.bitmap.get_loaders(device=args.device, task=args.task)
   else:
     train_loader, val_loader, calibration_loader, test_loader = etl.torch.dataset.get_loaders(device=args.device, task=args.task)
-    BUILDER = models.registry.lookup_model(args.model)
-  if args.test:
-    for model_class, train_batch, val_batch in zip(
-      models.registry.get_all_1d_models(),
-      train_loader, val_loader):
-      experiment = wrappers.experiment.Experiment(
-        model_class=model_class,
-        train_loader=[train_batch],
-        val_loader=[val_batch],
-        test_loader=None,
-        args=args)
-      print(f"starting test {model_class=}")
-      metric, epoch_loss_history = experiment.train(warmup_epochs=0, max_epochs=1)
-      print(f"finished test {model_class=} {metric=}")
-  elif args.coef:
+  if args.coef:
     # check coefficients
+    BUILDER = models.linear_bnc.Linear
     wrappers.coef.get_coef_dist(
       builder=BUILDER(n_classes=2, device=args.device),
       train_loader=train_loader,
       val_loader=val_loader)
   else:
-    use_experiment(args, BUILDER, train_loader, val_loader, calibration_loader, test_loader)
-
-def use_experiment(args, BUILDER, train_loader, val_loader, calibration_loader, test_loader):
+    BUILDER = models.linear_bnc.Linear
     experiment = wrappers.experiment.Experiment(
       model_class=BUILDER,
       train_loader=train_loader,
@@ -107,17 +82,7 @@ def use_experiment(args, BUILDER, train_loader, val_loader, calibration_loader, 
       calibration_loader=calibration_loader,
       test_loader=test_loader,
       args=args)
-    if args.tune:
-      # tune parameters
-      experiment.tune()
-    elif args.find_lr:
-      if args.disk == "save":
-        metric, epoch_loss_history = experiment.train(scheduler=None, **args.config)
-        plot.tune.plot_epoch_loss_history(args, epoch_loss_history)
-        plt.show()
-      else:
-        experiment.find_momentum(momentum=[0.9])
-    elif args.profile:
+    if args.profile:
       with cProfile.Profile() as pr:
         experiment.train()
       pr.dump_stats('results/output_file.prof')
@@ -129,25 +94,50 @@ def use_experiment(args, BUILDER, train_loader, val_loader, calibration_loader, 
 
 def compare(args, experiment):
       axs = None
-      if args.compare:
-        setups = args.compare
+      if args.config:
+        setups = util.config.get_setups(args.config)
       else:
-        setups = {"base": args.config["base"] if "base" in args.config else {}}
+        setups = {"nonce": {}}
       plt.ion()
-      running_loss_history = []
-      for k, v in setups.items():
-        metric, epoch_loss_history = experiment.train(**v)
+      y_axis_history = []
+      tuning_history = []
+      metric_history = []
+      for i, (k, v) in enumerate(setups.items()):
+        tqdm_prefix=f"Tuning[{i+1}/{len(setups)}]={k}"
+        metric, epoch_loss_history = experiment.train(tqdm_prefix=tqdm_prefix, **v)
         if args.log != "":
           experiment.log_training(epoch_loss_history, k)
-        if args.history != "none":
-          axs = plot.tune.plot_epoch_loss_history(args, epoch_loss_history, axs=axs, label=k)
-          running_loss_history += [epoch_loss_history]
+        if args.history == "variance":
+          logits, targets = experiment.batch_eval_test()
+          auc = sklearn.metrics.roc_auc_score(targets, logits)
+          probs = scipy.special.expit(logits)
+          brier = sklearn.metrics.brier_score_loss(targets, probs)
+          metric_history += [dict(auc=auc, brier=brier)]
+          tuning_history += [v]
+        elif args.history in "train val".split():
+          axs = plot.history.plot_history(args, epoch_loss_history, axs=axs, label=k)
+          y_axis_history += [epoch_loss_history]
         elif args.task in 'classify classify_patient classify_section'.split():
           axs = experiment.plot_trained(axs, label=k)
         plt.pause(0.1)
       plt.ioff()
-      if args.history != "none":
-        plot.tune.set_ylim(np.concatenate(running_loss_history))
+      if args.history in "train val".split():
+        plot.tune.set_ylim(np.concatenate(y_axis_history))
+      elif args.history == "variance":
+        #print(metric_history)
+        metrics = pd.DataFrame(metric_history)
+        #print(metrics)
+        auc = metrics.auc.copy()
+        #print(auc)
+        metrics = metrics.aggregate(["mean", "std"], axis=0).transpose()
+        print(" & ".join(metrics.columns))
+        metrics = [
+          f"${v['mean']:.3f} \pm {v['std']:.3f}$"
+          for _, v in metrics.iterrows()
+        ]
+        print(" & ".join(metrics))
+        plot.history.plot_best_values(pd.DataFrame(tuning_history), auc, task=args.task)
+        exit(0)
       suptitle = "Aggregated at the Box Level, not Patient" if args.task == "classify" else "Aggregated at Patient Level, not Box"
       plt.suptitle(suptitle)
       try:
