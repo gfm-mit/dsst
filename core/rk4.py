bugfix: don't do gradient ascent in rk4
also some nonsense involving defaults

core/rk4.py
@@ -4,7 +4,7 @@ import pytorch_optimizer
 
 class RK4(torch.optim.Optimizer, pytorch_optimizer.base.optimizer.BaseOptimizer):
     def __init__(self, params, lr=1e-2):
-        super().__init__(params)
+        super().__init__(params, dict(lr=lr))
         self.learning_rate = lr
 
     def __str__(self) -> str:
@@ -19,7 +19,7 @@ class RK4(torch.optim.Optimizer, pytorch_optimizer.base.optimizer.BaseOptimizer)
         assert get_grads is not None
 
         with torch.enable_grad():
-          get_grads()
+          loss = get_grads()
 
         for group in self.param_groups:
           for p in group['params']:
@@ -27,7 +27,7 @@ class RK4(torch.optim.Optimizer, pytorch_optimizer.base.optimizer.BaseOptimizer)
               continue
             self.state[p]['x0'] = p.clone()
             self.state[p]['k'] = p.grad.clone()
-            p.add_(p.grad / 2)
+            p.add_(-p.grad / 2)
 
         with torch.enable_grad():
           get_grads()
@@ -39,7 +39,7 @@ class RK4(torch.optim.Optimizer, pytorch_optimizer.base.optimizer.BaseOptimizer)
 
             self.state[p]['k'] += 2 * p.grad
             p.data = self.state[p]['x0']
-            p.add_(p.grad / 2)
+            p.add_(-p.grad / 2)
 
         with torch.enable_grad():
           get_grads()
@@ -51,7 +51,7 @@ class RK4(torch.optim.Optimizer, pytorch_optimizer.base.optimizer.BaseOptimizer)
 
             self.state[p]['k'] += 2 * p.grad
             p.data = self.state[p]['x0']
-            p.add_(p.grad)
+            p.add_(-p.grad)
 
         with torch.enable_grad():
           get_grads()
@@ -62,4 +62,5 @@ class RK4(torch.optim.Optimizer, pytorch_optimizer.base.optimizer.BaseOptimizer)
               continue
 
             self.state[p]['k'] += p.grad
-            p.data = self.state[p]['x0'] + self.state[p]['k'] / 6 * self.lr
+            p.data = self.state[p]['x0'] - self.state[p]['k'] / 6 * self.learning_rate
+        return loss