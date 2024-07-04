0e44c7f8949a64cbefdda40bb45a85634a5e89af
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-07-03 20:58:04 -0400

    bugfix: I implemented RK4 wrong
    Luca would kill me

--- core/rk4.py
+++ core/rk4.py
@@ -1,11 +1,15 @@
+import numpy as np
 import torch
 import torch.optim
 import pytorch_optimizer
 
+import pandas as pd
+
 class RK4(torch.optim.Optimizer, pytorch_optimizer.base.optimizer.BaseOptimizer):
-    def __init__(self, params, lr=1e-2):
+    def __init__(self, params, named, lr=1e-2):
         super().__init__(params, dict(lr=lr))
-        self.learning_rate = lr
+        for k, v in named:
+          self.state[v]['name'] = k
 
     def __str__(self) -> str:
         return 'RK4'
@@ -16,51 +20,55 @@ class RK4(torch.optim.Optimizer, pytorch_optimizer.base.optimizer.BaseOptimizer)
 
     @torch.no_grad()
     def step(self, get_grads=None):
-        assert get_grads is not None
+      assert get_grads is not None
 
-        with torch.enable_grad():
+      with torch.enable_grad():
           loss = get_grads()
 
+      with torch.no_grad():
         for group in self.param_groups:
           for p in group['params']:
             if p.grad is None:
               continue
             self.state[p]['x0'] = p.clone()
-            self.state[p]['k'] = p.grad.clone()
-            p.add_(-p.grad / 2)
+            self.state[p]['dx0'] = p.grad * group['lr']
 
-        with torch.enable_grad():
+            self.state[p]['dx'] = 1 / 6 * p.grad * group['lr']
+            p.add_(-0.5 * p.grad * group['lr'])
+
+      with torch.enable_grad():
           get_grads()
 
+      with torch.no_grad():
         for group in self.param_groups:
           for p in group['params']:
             if p.grad is None:
               continue
 
-            self.state[p]['k'] += 2 * p.grad
-            p.data = self.state[p]['x0']
-            p.add_(-p.grad / 2)
+            self.state[p]['dx'].add_(1 / 3 * p.grad * group['lr'])
+            p.data = self.state[p]['x0'] - 0.5 * p.grad * group['lr']
 
-        with torch.enable_grad():
+      with torch.enable_grad():
           get_grads()
 
+      with torch.no_grad():
         for group in self.param_groups:
           for p in group['params']:
             if p.grad is None:
               continue
 
-            self.state[p]['k'] += 2 * p.grad
-            p.data = self.state[p]['x0']
-            p.add_(-p.grad)
+            self.state[p]['dx'].add_(1 / 3 * p.grad * group['lr'])
+            p.data = self.state[p]['x0'] - p.grad * group['lr']
 
-        with torch.enable_grad():
+      with torch.enable_grad():
           get_grads()
 
+      with torch.no_grad():
         for group in self.param_groups:
           for p in group['params']:
             if p.grad is None:
               continue
 
-            self.state[p]['k'] += p.grad
-            p.data = self.state[p]['x0'] - self.state[p]['k'] / 6 * self.learning_rate
-        return loss
+            self.state[p]['dx'] += 1 / 6 * p.grad * group['lr']
+            p.data = self.state[p]['x0'] - self.state[p]['dx']
+      return loss
