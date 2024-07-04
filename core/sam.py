52db3a26adba739c772cabbe180314dec0bd8547
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-07-04 10:09:41 -0400

    feature: create sam.py
    the one in pytorch_optimizer doesn't handle batch norm correctly, apparently

---
+++ core/sam.py
@@ -0,0 +1,23 @@
+import torch
+import pytorch_optimizer.optimizer.sam
+from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, OPTIMIZER, PARAMETERS
+from pytorch_optimizer.base.exception import NoClosureError
+
+import models.bn_utils
+
+class SAM(pytorch_optimizer.optimizer.sam.SAM):
+  @torch.no_grad()
+  def step(self, closure: CLOSURE = None):
+    if closure is None:
+      raise NoClosureError(str(self))
+
+    with torch.enable_grad():
+      loss = closure()
+    self.first_step(zero_grad=True)
+
+    models.bn_utils.disable_running_stats(self.params)
+    with torch.enable_grad():
+      closure()
+    self.second_step()
+    models.bn_utils.enable_running_stats(self.params)
+    return loss
