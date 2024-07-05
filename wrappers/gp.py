477027c48260324eb07f851fd002850a2ba23eee
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-07-05 18:19:03 -0400

    Update gp.py

--- wrappers/gp.py
+++ wrappers/gp.py
@@ -10,21 +10,27 @@ import core.metrics
 def update_mean(mean, n, value):
   return (n * mean + value) / (n + 1)
 
-def gp(args, experiment):
-  K = "learning_rate"
+def gp(args, experiment, K="learning_rate", scale="log", budget=None):
   assert args.config
+  to_gp_space = lambda x: x
+  if scale == "log":
+    to_gp_space = np.log
   setups = util.config.parse_config(args.config)
   assert not setups.duplicated().any()
   stats = pd.DataFrame(columns="X Y S".split())
-  X_gpr = np.linspace(np.log(setups[K].min()), np.log(setups[K].max()), 100)[:, None]
-  plt.ion()
-  high, low = None, None
-  kernel = sklearn.gaussian_process.kernels.RBF(length_scale_bounds=[3e-1,1e0]
+  low, high = setups[K].min(), setups[K].max()
+  if scale == "log":
+    X_gpr = np.geomspace(low, high, 100)[:, None]
+  else:
+    X_gpr = np.linspace(low, high, 100)[:, None]
+  log, high = None, None
+  kernel = sklearn.gaussian_process.kernels.RBF(length_scale_bounds=[3e-1,3e0]
     ) + sklearn.gaussian_process.kernels.WhiteKernel(noise_level_bounds=[1e-1,1e1])
   gpr = sklearn.gaussian_process.GaussianProcessRegressor(
     kernel=kernel, random_state=0, normalize_y=True, n_restarts_optimizer=10)
   fig, axs = plt.subplots(2, sharex=True)
-  for iter in range(setups.shape[0]):
+  plt.ion()
+  for iter in range(budget):
     params = setups.iloc[iter % setups.shape[0]]
     metric, epoch_loss_history = experiment.train(tqdm_prefix=None, **params.to_dict())
     steps = core.metrics.argbest(epoch_loss_history, args.task)
@@ -35,14 +41,15 @@ def gp(args, experiment):
     plt.scatter(stats.X, stats.Y, color="black")
 
     # Define the kernel: RBF for smooth changes, WhiteKernel for noise level
-    gpr.fit(np.log(stats.X.values[:, None]), stats.Y)
+    gpr.fit(to_gp_space(stats.X.values)[:, None], stats.Y)
     print(gpr.kernel_)
-    Y_gpr, S_gpr = gpr.predict(X_gpr, return_std=True)
+    Y_gpr, S_gpr = gpr.predict(to_gp_space(X_gpr), return_std=True)
     if high is None:
-      high, = plt.plot(np.exp(X_gpr), Y_gpr + 2 * S_gpr)
-      low, = plt.plot(np.exp(X_gpr), Y_gpr - 2 * S_gpr)
-      plt.xscale('log')
-      plt.ylim([.4, .85])
+      high, = plt.plot(X_gpr, Y_gpr + 2 * S_gpr)
+      low, = plt.plot(X_gpr, Y_gpr - 2 * S_gpr)
+      if scale == "log":
+        plt.xscale('log')
+      plt.ylim([.6, .85])
     else:
       high.set_ydata(Y_gpr + 2 * S_gpr)
       low.set_ydata(Y_gpr - 2 * S_gpr)
