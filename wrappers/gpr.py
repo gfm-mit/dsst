8caa5f4760a9bc19652174b52e15e4ae5ae6bb15
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-07-06 23:26:02 -0400

    feature: get best guess from GPR
    I wonder if I'm supposed to use log(n)/n_i instead

--- wrappers/gpr.py
+++ wrappers/gpr.py
@@ -5,18 +5,21 @@ import sklearn.gaussian_process
 import sklearn.metrics.pairwise
 import matplotlib.pyplot as plt
 
+import core.metrics
+
 
 class GPR:
-  def __init__(self, K, scale, budget, min, max, sd=None):
+  def __init__(self, K, scale, budget, min, max, task="next_token"):
     self.K = K
     self.scale = scale
     self.budget = budget
+    self.task=task
     kernel = sklearn.gaussian_process.kernels.RationalQuadratic(
       length_scale=0.2,
       length_scale_bounds='fixed',
       alpha_bounds=[1e-1, 1e1],
       ) + sklearn.gaussian_process.kernels.WhiteKernel()
-    self.mean_gpr = sklearn.gaussian_process.GaussianProcessRegressor(
+    self.gpr = sklearn.gaussian_process.GaussianProcessRegressor(
       kernel=kernel, random_state=0, normalize_y=True, n_restarts_optimizer=10)
 
     if scale == "log":
@@ -34,23 +37,30 @@ class GPR:
     old_format = warnings.formatwarning
     warnings.formatwarning = lambda message, category, filename, lineno, line: f"MEAN-GPR: {message}\n"
 
-    self.mean_gpr.fit(self.to_gp_space(stats.X.values)[:, None], stats.Y)
-    self.Y, self.S = self.mean_gpr.predict(self.to_gp_space(self.X), return_std=True)
-    print(f"{self.mean_gpr.kernel_=}")
+    self.gpr.fit(self.to_gp_space(stats.X.values)[:, None], stats.Y)
+    self.Y, self.S = self.gpr.predict(self.to_gp_space(self.X), return_std=True)
+    print(f"{self.gpr.kernel_=}")
 
-    XY = self.mean_gpr.kernel_.k1(
+    XY = self.gpr.kernel_.k1(
       self.to_gp_space(stats.X.values[:, None]),
       self.to_gp_space(self.X),
       )
     XY = XY.sum(axis=0)
     self.S2 = self.S / np.sqrt(XY)
 
+    self.X_best = self.X[
+     core.metrics.argbest(
+      (self.Y + 2 * self.S2).tolist(),
+      task=self.task
+    )]
+
     warnings.formatwarning = old_format
 
   def make_plot(self, axs):
     plt.sca(axs[0])
-    self.band = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S, self.Y - 2 * self.S, alpha=0.1, zorder=-20)
-    self.band2 = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S2, self.Y - 2 * self.S2, alpha=0.5, zorder=+10)
+    self.band = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S, self.Y - 2 * self.S, alpha=0.1, zorder=-10)
+    self.band2 = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S2, self.Y - 2 * self.S2, alpha=0.5, zorder=10)
+    self.vline = plt.axvline(x=self.X_best, color="lightgray", linestyle=':', zorder=-20)
     if self.scale == "log":
       plt.xscale('log')
     elif self.scale == "log1p":
@@ -65,9 +75,10 @@ class GPR:
       return self.make_plot(axs)
     plt.sca(axs[0])
     self.band.remove()
-    self.band = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S, self.Y - 2 * self.S, alpha=0.1, zorder=-20)
+    self.band = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S, self.Y - 2 * self.S, alpha=0.1, zorder=-10)
     self.band2.remove()
-    self.band2 = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S2, self.Y - 2 * self.S2, alpha=0.5, zorder=+10)
+    self.band2 = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S2, self.Y - 2 * self.S2, alpha=0.5, zorder=10)
+    self.vline.set_xdata(self.X_best)
   
   def scatter(self, stats, axs):
     plt.sca(axs[0])
