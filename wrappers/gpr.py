85b6b936a84c90ebe50dbe174e6600e2aa6100c0
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-07-06 23:07:51 -0400

    feature: gpr: uncertainty band for mean
    the sqrt(1/n) bit

--- wrappers/gpr.py
+++ wrappers/gpr.py
@@ -7,22 +7,17 @@ import matplotlib.pyplot as plt
 
 
 class GPR:
-  def __init__(self, K, scale, budget, min, max, sd=0.01):
+  def __init__(self, K, scale, budget, min, max, sd=None):
     self.K = K
     self.scale = scale
     self.budget = budget
-    self.sd = sd
-    k1 = sklearn.gaussian_process.kernels.RationalQuadratic(length_scale=0.05, length_scale_bounds='fixed'
-      ) + sklearn.gaussian_process.kernels.WhiteKernel(noise_level_bounds=[1e-1 * sd, 1e1 * sd])
+    kernel = sklearn.gaussian_process.kernels.RationalQuadratic(
+      length_scale=0.2,
+      length_scale_bounds='fixed',
+      alpha_bounds=[1e-1, 1e1],
+      ) + sklearn.gaussian_process.kernels.WhiteKernel()
     self.mean_gpr = sklearn.gaussian_process.GaussianProcessRegressor(
-      kernel=k1, random_state=0, normalize_y=True, n_restarts_optimizer=10)
-    k2 = sklearn.gaussian_process.kernels.RBF(length_scale_bounds=[.1,2]
-      ) + sklearn.gaussian_process.kernels.WhiteKernel(
-      noise_level=1.23,
-      noise_level_bounds="fixed"
-      )
-    self.var_gpr = sklearn.gaussian_process.GaussianProcessRegressor(
-      kernel=k2, random_state=0, n_restarts_optimizer=10)
+      kernel=kernel, random_state=0, normalize_y=True, n_restarts_optimizer=10)
 
     if scale == "log":
       self.to_gp_space = np.log10
@@ -41,39 +36,21 @@ class GPR:
 
     self.mean_gpr.fit(self.to_gp_space(stats.X.values)[:, None], stats.Y)
     self.Y, self.S = self.mean_gpr.predict(self.to_gp_space(self.X), return_std=True)
-    E = self.mean_gpr.predict(self.to_gp_space(stats.X.values)[:, None])
-    V = (stats.Y - E)**2
-    # TODO: consider fitting raw variance, but adding white kernel
-    LV = np.log(V / self.sd + 1e-2)
-    warnings.formatwarning = lambda message, category, filename, lineno, line: f"VAR-GPR: {message}\n"
-    self.var_gpr.fit(self.to_gp_space(stats.X.values)[:, None], LV)
-    self.LV = self.var_gpr.predict(self.to_gp_space(self.X))
-    self.S2 = np.sqrt(self.sd * np.exp(self.LV))
-    print(f"mean={self.mean_gpr.kernel_}\nvar={self.var_gpr.kernel_}")
+    print(f"{self.mean_gpr.kernel_=}")
 
-    XY = self.mean_gpr.kernel_(
-      stats.X.values[:, None],
-      self.X,
+    XY = self.mean_gpr.kernel_.k1(
+      self.to_gp_space(stats.X.values[:, None]),
+      self.to_gp_space(self.X),
       )
-    #XX_inv = np.linalg.inv(self.mean_gpr.kernel_(
-    #  stats.X.values[:, None],
-    #))
-    #XY = XX_inv @ XY
     XY = XY.sum(axis=0)
-    self.XY = np.sqrt(np.mean(V)/XY)
+    self.S2 = self.S / np.sqrt(XY)
 
     warnings.formatwarning = old_format
 
   def make_plot(self, axs):
     plt.sca(axs[0])
-    #self.med, = plt.plot(self.X, self.Y)
-    self.band = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S, self.Y - 2 * self.S, alpha=0.3, zorder=-100)
-    self.high, = plt.plot(self.X, self.Y + 2 * self.S2)
-    self.low, = plt.plot(self.X, self.Y - 2 * self.S2)
-    self.high.set_color(self.low.get_color())
-    self.high2, = plt.plot(self.X, self.Y + 2 * self.XY)
-    self.low2, = plt.plot(self.X, self.Y - 2 * self.XY)
-    self.high2.set_color(self.low2.get_color())
+    self.band = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S, self.Y - 2 * self.S, alpha=0.1, zorder=-20)
+    self.band2 = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S2, self.Y - 2 * self.S2, alpha=0.5, zorder=+10)
     if self.scale == "log":
       plt.xscale('log')
     elif self.scale == "log1p":
@@ -87,13 +64,10 @@ class GPR:
       self.interactive = True
       return self.make_plot(axs)
     plt.sca(axs[0])
-    #self.med.set_ydata(self.Y)
     self.band.remove()
-    self.band = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S, self.Y - 2 * self.S, alpha=0.1, zorder=-100)
-    self.high.set_ydata(self.Y + 2 * self.S2)
-    self.low.set_ydata(self.Y - 2 * self.S2)
-    self.high2.set_ydata(self.Y + 2 * self.XY)
-    self.low2.set_ydata(self.Y - 2 * self.XY)
+    self.band = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S, self.Y - 2 * self.S, alpha=0.1, zorder=-20)
+    self.band2.remove()
+    self.band2 = plt.fill_between(self.X[:, 0], self.Y + 2 * self.S2, self.Y - 2 * self.S2, alpha=0.5, zorder=+10)
   
   def scatter(self, stats, axs):
     plt.sca(axs[0])
