47f6750bc6219b071d7d6a91ad3a1e3764a8318f
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-07-09 10:32:38 -0400

    Update base.py

--- bandit/base.py
+++ bandit/base.py
@@ -6,31 +6,38 @@ import util.config
 import core.metrics
 
 class Bandit:
-  def __init__(self, args: dict, conf: dict, options: pd.DataFrame):
+  def __init__(self, args: dict, conf: dict, arms: pd.DataFrame):
     super().__init__()
     self.args = args
-    pd.Series(self.args).to_csv("results/bandit/args.csv")
+    pd.Series(self.args).to_csv("rewards/bandit/args.csv")
     self.conf = conf
-    pd.Series(self.conf).to_csv("results/bandit/conf.csv")
-    self.options = options.reset_index(drop=True)
-    self.options.to_csv("results/bandit/options.csv")
-    self.results = None
+    pd.Series(self.conf).to_csv("rewards/bandit/conf.csv")
+    self.arms = arms.reset_index(drop=True)
+    self.arms.to_csv("rewards/bandit/arms.csv")
+    self.rewards = None
   
-  def suggest_option(self):
-    if self.results.shape[0] > self.conf["budget"]:
+  def suggest_arm(self):
+    if self.rewards.shape[0] >= self.conf["budget"]:
       return None
-    self.update_state()
+    self.state = self.calculate_state()
     self.idx = self.state.n.idxmin()
-    return self.options.loc[self.idx]
+    return self.arms.loc[self.idx]
   
-  def update_results(self, metric):
-    if self.results is None:
-      self.results = metric.rename(0).to_frame().transpose()
-      self.results["option_idx"] = self.idx
+  def update_rewards(self, metric):
+    if self.rewards is None:
+      self.rewards = metric.rename(0).to_frame().transpose()
+      self.rewards["arm_idx"] = self.idx
     else:
-      self.results.loc[self.results.index.max() + 1] = dict(**metric, option_idx=self.idx)
-    self.results.to_csv("results/bandit/results.csv")
+      self.rewards.loc[self.rewards.index.max() + 1] = dict(**metric, arm_idx=self.idx)
+    self.rewards.to_csv("rewards/bandit/rewards.csv")
   
-  def update_state(self): 
-    counts = self.results.groupby("option_idx").size().rename("n")
-    self.state = counts.reindex(self.options.index, fill_value=0)
+  def calculate_state(self): 
+    counts = self.rewards.groupby("arm_idx").size().rename("n")
+    return counts.reindex(self.arms.index, fill_value=0)
+
+  def get_label(self):
+    label = {}
+    for k in self.arms.columns:
+      if self.arms[k].nunique() != 1:
+        label[k] = self.arms.loc[self.idx, k]
+    return util.config.pprint_dict(label)
