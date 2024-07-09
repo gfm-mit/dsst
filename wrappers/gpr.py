884385855d745b9ddbd67bc5e707df31fd52b08b
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-07-09 17:14:14 -0400

    linter: get rid of unused budget argument to GPR

--- wrappers/gpr.py
+++ wrappers/gpr.py
@@ -10,11 +10,10 @@ import core.metrics
 
 
 class GPR:
-  def __init__(self, K, scale, budget, task=None, sigma=0.1, **kwargs):
+  def __init__(self, K, scale, task=None, sigma=0.1, **kwargs):
     print(f"{kwargs=}")
     self.K = K
     self.scale = scale
-    self.budget = budget
     assert task is not None
     self.task=task
     kernel = sklearn.gaussian_process.kernels.RationalQuadratic(
